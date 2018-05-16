cd176858c72ef973cfacb5c949e7c44b907532df
ripple <zhouripple@163.com>
2018-05-16 21:41:39 +0800

    个人发起页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -385,5 +385,22 @@ Page({
    * 用户点击右上角分享分享
    */
   onShareAppMessage: function () {
+  },
+  onLaunch: function () {
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://test.com/onLogin',
+            data: {
+              code: res.code
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    });
   }
 })
---
+++ pages/mypost/mypost.js
@@ -0,0 +1,83 @@
+// pages/mypost/mypost.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    personLink: {
+      url01: '/static/images/个人背景无阴影@2x.png',
+      url02: '/static/images/个人背景无阴影@3x.png'
+    },
+    details:[
+      {
+        id:1,
+        title:'江南小镇（砂之船奥莱店）',
+        url:'/static/images/food.png'
+      },
+      {
+        id: 2,
+        title: '金鹰',
+        url: '/static/images/food.png'
+      }
+    
+    ]
+      
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
